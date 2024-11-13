b727da14c991a0e09420277155c232a806d11ce2
Jan20199 <jan20199@email.vccs.edu>
2024-11-13 13:07:42 -0700

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new("Red", 3);
+        Console.WriteLine($" Color: {square.GetColor()} \n Area: {square.GetArea()}");
+
+        Rectangle rectangle = new("Blue", 4, 5);
+        Console.WriteLine($" Color: {rectangle.GetColor()} \n Area: {rectangle.GetArea()}");
+       
+        Circle circle = new("Green", 6);
+        Console.WriteLine($" Color: {circle.GetColor()} \n Area: {circle.GetArea()}");
+
     }
 }
